d23e95f564aeec5d63a5806b0c388aa811aa1f9e
dags- <melhuishrob@gmail.com>
2015-01-04 16:39:19 +0000

    1.7.10v2.1

--- java/me/dags/daflight/gui/uielements/EntryBox.java
+++ java/me/dags/daflight/gui/uielements/EntryBox.java
@@ -1,17 +1,26 @@
 /*
  * Copyright (c) 2014, dags_ <dags@dags.me>
  *
- *  Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby
- *  granted, provided that the above copyright notice and this permission notice appear in all copies.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING
- *  ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL,
- *  DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
- *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE
- *  USE OR PERFORMANCE OF THIS SOFTWARE.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
-package me.dags.daflight.minecraft.uielements;
+package me.dags.daflight.gui.uielements;
 
 import me.dags.daflight.gui.UIElement;
 import me.dags.daflight.minecraft.Colour;
@@ -26,6 +35,7 @@ public class EntryBox extends GuiTextField implements UIElement
     private ToolTip toolTip;
     private boolean hovered;
     private String label;
+    private String defaultValue;
     private boolean coloured = true;
     private boolean isActive;
     private int xPos;
@@ -33,7 +43,7 @@ public class EntryBox extends GuiTextField implements UIElement
     private int height;
     private int defaultY;
 
-    public EntryBox(int x, int y, int width, int height, String label, boolean colour)
+    public EntryBox(int x, int y, int width, int height, String label, String defaultValue, boolean colour)
     {
         super(MCGame.getMinecraft().fontRendererObj, x, y, width, height);
         this.setFocused(false);
@@ -44,6 +54,7 @@ public class EntryBox extends GuiTextField implements UIElement
         this.height = height;
         this.defaultY = y;
         this.label = label + ": ";
+        this.defaultValue = defaultValue;
     }
 
     public EntryBox setString(String s)
@@ -82,7 +93,7 @@ public class EntryBox extends GuiTextField implements UIElement
     public void drawElement(int mouseX, int mouseY)
     {
         super.drawTextBox();
-        hovered = mouseX >= this.xPos && mouseX <= this.xPos + this.getWidth() && mouseY >= this.yPos && mouseY <= this.height;
+        hovered = mouseX >= this.xPos && mouseX <= this.xPos + this.getWidth() && mouseY >= this.yPos && mouseY <= this.yPos + this.height;
     }
 
     @Override
@@ -115,6 +126,14 @@ public class EntryBox extends GuiTextField implements UIElement
             this.unsetActive();
         }
         return false;
+    }
+
+    @Override
+    public boolean shiftClicked()
+    {
+        if (this.hovered)
+            this.setString(this.defaultValue);
+        return this.hovered;
     }
 
     @Override
